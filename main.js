0bfadee51c880f0456a200f04a0d1a2455927c50
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-06-10 23:14:21 +0530

    implemented bfs function

---
+++ main.js
@@ -0,0 +1,14 @@
+const { bfs } = require('./src/graph');
+const data = require('./src/data.json');
+
+const main = function () {
+  console.log(`1) bb to jj`, bfs(data, 'bb', 'jj'));
+  console.log(`2) jj to aa`, bfs(data, 'jj', 'aa'));
+  console.log(`3) aa to hh`, bfs(data, 'aa', 'hh'));
+  console.log(`4) hh to ii`, bfs(data, 'hh', 'ii'));
+  console.log(`5) ii to ee`, bfs(data, 'ii', 'ee'));
+  console.log(`6) ee to mm`, bfs(data, 'ee', 'mm'));
+  console.log(`7) mm to jj`, bfs(data, 'mm', 'jj'));
+};
+
+main();
--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,31 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const addDestinationsInQueue = function (pairs, queue, visited, destination) {
+  pairs.forEach((transport) => {
+    if (
+      transport[0] === destination &&
+      !visited.includes(transport[1]) &&
+      !queue.includes(transport[1])
+    ) {
+      queue.push(transport[1]);
+    }
+  });
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+  let queue = [source];
+  let visited = [];
+  while (queue.length) {
+    let destination = queue.shift();
+    if (destination === target) return true;
+    visited.push(destination);
+    addDestinationsInQueue(pairs, queue, visited, destination);
+  }
+  return false;
+};
+
+module.exports = { bfs };
