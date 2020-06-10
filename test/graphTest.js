28fa3245ebce606bea052c65fc4ab1d70f2ae56f
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-06-10 23:48:30 +0530

    added tests for the bfs

---
+++ test/graphTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const { bfs } = require('../src/graph');
+const data = require('../src/data.json');
+
+describe('bfs', function () {
+  it('should return true when path exists', function () {
+    assert.isTrue(bfs(data, 'jj', 'aa'));
+  });
+  it('should return false when path not exists', function () {
+    assert.isNotTrue(bfs(data, 'bb', 'jj'));
+  });
+});
