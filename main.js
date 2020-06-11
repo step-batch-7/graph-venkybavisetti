c8c6a598ae6d6767dea9708dfe841044b96e627e
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-06-11 10:43:33 +0530

    fixed bugs in the bfs function

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { bfs } = require('./src/graph');
 const data = require('./src/data.json');
 
+const duplicate = [
+  ['aa', 'bb'],
+  ['bb', 'cc'],
+];
 const main = function () {
   console.log(`1) bb to jj`, bfs(data, 'bb', 'jj'));
   console.log(`2) jj to aa`, bfs(data, 'jj', 'aa'));
@@ -8,7 +12,7 @@ const main = function () {
   console.log(`4) hh to ii`, bfs(data, 'hh', 'ii'));
   console.log(`5) ii to ee`, bfs(data, 'ii', 'ee'));
   console.log(`6) ee to mm`, bfs(data, 'ee', 'mm'));
-  console.log(`7) mm to jj`, bfs(data, 'mm', 'jj'));
+  console.log(`7) mm to jj`, bfs(duplicate, 'bb', 'bb'));
 };
 
 main();
